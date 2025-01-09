e5e676096fe42a3bc3a3c0938883f71fd7b3d763
hieucoderdzai <iwillwin2211@gmail.com>
2025-01-09 15:09:59 +0700

    Update Baseball.java

--- src/main/java/com/example/demo/common/Baseball.java
+++ src/main/java/com/example/demo/common/Baseball.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 public class Baseball implements Coach{
     public Baseball() {
-        System.out.println("In constructor " + getClass().getSimpleName());
+        System.out.println("Hello " + getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
