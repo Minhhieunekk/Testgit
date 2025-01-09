8e9b30a4189d6b76846a4073a0686970fec66b1a
hieu <iwillwin2211@gmail.com>
2025-01-09 15:22:44 +0700

    update_now_conflict

--- src/main/java/com/example/demo/common/CricketCoach.java
+++ src/main/java/com/example/demo/common/CricketCoach.java
@@ -15,7 +15,7 @@ public class CricketCoach implements Coach{
     }
     public  String abc() {
         System.out.println("In abc " + getClass().getSimpleName());
-        return "Test " + getClass().getSimpleName();
+        return "Test 123" + getClass().getSimpleName();
     }
 
     @PostConstruct
