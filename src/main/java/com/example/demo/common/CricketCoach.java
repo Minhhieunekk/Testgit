459f14914fa3a08c9ca3331cd3ca9281d8b02112
hieu <iwillwin2211@gmail.com>
2025-01-09 16:13:15 +0700

    Revert "conflict_3"
    This reverts commit dcd9a38ef7333ce1a52a3fd8aa467778775db59e.

--- src/main/java/com/example/demo/common/CricketCoach.java
+++ src/main/java/com/example/demo/common/CricketCoach.java
@@ -15,7 +15,7 @@ public class CricketCoach implements Coach{
     }
     public  String abc() {
         System.out.println("In abc " + getClass().getSimpleName());
-        return "Test conflict_3" + getClass().getSimpleName();
+        return "Test conflict_2" + getClass().getSimpleName();
     }
 
     @PostConstruct
